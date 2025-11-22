f013041a6df92f8268f009ffbfe8e92053e9cc0f
Leandro Osuna <leandrog.osuna@gmail.com>
2025-11-21 22:24:37 -0300

    sound lib switched to irrklang, basic 2D and 3D sound api

--- SilkGameCore/Sound/SoundManager.cs
+++ SilkGameCore/Sound/SoundManager.cs
@@ -1,58 +1,102 @@
-﻿using ManagedBass;
+﻿using IrrKlang;
+using System.Numerics;
 namespace SilkGameCore.Sound
 {
-    public class SoundManager
+    public class SoundManager : IDisposable
     {
+        ISoundEngine _engine;
+        private List<Sound> _sounds = new List<Sound>();
+        
+        private Vector3D _pos = new Vector3D(0, 0, 0);
+        private Vector3D _dir = new Vector3D(1, 0, 0);
+       
         public SoundManager()
         {
+            _engine = new ISoundEngine();
 
+            Set3DMinMaxDistance(0.5f, 200f);
+            SetRollOff(.5f);
+            SetDopplerFactors(1.0f, 1.0f);
+        }
+        public void Update()
+        {
+            _sounds.ForEach(s => s._instances.RemoveAll(i => i.IsFinished));
         }
 
-        static int currentStream;
-        public void Play(string path)
+        public Sound Add(string path, bool allowMultiple = true)
         {
-            if (!Bass.Init())
-            {
-                Console.WriteLine("Sound manager init fail");
-
-                return;
-            }
-            currentStream = Bass.CreateStream(path, 0, 0, BassFlags.Default);
-            if (currentStream == 0)
-            {
-                Console.WriteLine("Failed to load audio file.");
-
-                Console.WriteLine($"last error {Bass.LastError}"); // Shows why it failed
-                return;
-            }
-            Bass.ChannelPlay(currentStream);
-
+            var src = _engine.GetSoundSource(path);
+            var sound = new Sound(src, allowMultiple);
+            _sounds.Add(sound);
+            return sound;
         }
 
-        public void Play3D(string path)
+        public Sound3D Add3D(string path, bool allowMultiple = true)
         {
-            if (!Bass.Init())
-            {
-                Console.WriteLine("Sound manager init fail");
-
-                return;
-            }
-            currentStream = Bass.CreateStream(path, 0, 0, BassFlags.Default);
-            if (currentStream == 0)
-            {
-                Console.WriteLine("Failed to load audio file.");
-
-                Console.WriteLine($"last error {Bass.LastError}"); // Shows why it failed
-                return;
-            }
-            Bass.ChannelPlay(currentStream);
-
+            var src = _engine.GetSoundSource(path);
+            var sound = new Sound3D(src, allowMultiple);
+            _sounds.Add(sound);
+            return sound;
         }
 
-        public void Stop()
+        public void Toggle(SoundInstance instance)
         {
-            Bass.StreamFree(currentStream);
-            Bass.Free();
+            instance.Paused = !instance.Paused;
+        }
+        public SoundInstance Play(Sound sound, bool loop = false, bool startPaused = false)
+        {
+            if (!_sounds.Contains(sound))
+                return null;
+
+            return sound.Play(ref _engine, loop, startPaused);
+        }
+        public SoundInstance3D Play(Sound3D sound, bool loop = false, bool startPaused = false)
+        {
+            if (!_sounds.Contains(sound))
+                return null;
+
+            return sound.Play(ref _engine, loop, startPaused);
+        }
+        public void Stop(SoundInstance sound)
+        {
+            sound.Stop();
+        }
+
+        public void SetListenerPositionDirection(Vector3 position, Vector3 direction, bool flipDirection = true)
+        {
+            _pos.Set(position.X, position.Y, position.Z);
+            
+            if(flipDirection)
+                _dir.Set(-direction.X, direction.Y, -direction.Z);
+            else
+                _dir.Set(direction.X, direction.Y, direction.Z);
+
+            _engine.SetListenerPosition(_pos, _dir);
+        }
+        public void Set3DMinMaxDistance(float min, float max)
+        {
+            _engine.Default3DSoundMinDistance = min;
+            _engine.Default3DSoundMaxDistance = max;
+            
+        }
+
+        public void SetRollOff(float rollOffFactor)
+        {
+            _engine.SetRolloffFactor(rollOffFactor);
+        }
+        public void SetDopplerFactors(float doppler, float distance)
+        {
+            _engine.SetDopplerEffectParameters(doppler, distance);
+        }
+        public void StopAll()
+        {
+            _engine.StopAllSounds();
+            
+        }
+
+        public void Dispose()
+        {
+            _engine.Dispose();
         }
     }
 }
